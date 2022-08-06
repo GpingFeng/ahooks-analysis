d5a518d5c271826fd2b0a6060b948b64dae4f290
Gopal <1172597655@qq.com>
2022-08-06 18:28:34 +0800

    docs: logo

--- .umirc.ts
+++ .umirc.ts
@@ -4,10 +4,8 @@ const repo = 'ahooks-analysis';
 
 export default defineConfig({
   title: 'ahooks analysis',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: '/images/logo.png',
+  logo: '/images/logo.png',
   outputPath: 'docs-dist',
   mode: 'site',
   hash: true,
